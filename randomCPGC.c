aa3281b5ac9bbf20025a14046996d75783014435
aksharc2 <aksharc22@gmail.com>
2023-11-30 21:24:18 -0500

    updated v3

--- randomCPGC.c
+++ randomCPGC.c
@@ -9,8 +9,8 @@ int main(int argc, char* argv[]) {
     // Check that only 2 MPI processes are spawned
     int comm_size;
     MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
-    int graphNodes = 5;
-	int k_hat;
+    int graphNodes = 8;
+	
     int arraySize = (int) ceil((double)(graphNodes * graphNodes)/(double)comm_size);
 
     // Get my rank
@@ -39,60 +39,97 @@ int main(int argc, char* argv[]) {
 	    printf("The array size is %d.\n", arraySize);
 		// Print the initial matrix
 		
-		for(int j = 0; j < comm_size; j++){
-			for (int i = 0; i < arraySize; i++) {  
-				printf("%d ", adjMatrix_buffer[j* comm_size + i]);
-			}
-			printf("\n");
+		for(int j = 0; j < comm_size * arraySize; j++){
+			printf("%d ", adjMatrix_buffer[j]);
+			if ((j+1)%graphNodes == 0)
+				printf("\n");
 		}
+		printf("\n");
+		int *edgesRemoved = (int*)malloc((comm_size) * sizeof(int));
+		int edgeRemoved = 0;
+		MPI_Request gRequest, sRequest;
 		
-		MPI_Request request;
-		int s = 0 ;
-		k_hat = 2;
-		MPI_Bcast(&k_hat, 1, MPI_INT, 0, MPI_COMM_WORLD);
-	    while( s < graphNodes){
-			for ( int r = 1; r < comm_size; r++){	      
-				int randomVertex = rand() % graphNodes;		    
-				printf("MPI process %d sends value %d to processor %d.\n", my_rank, randomVertex, r);
-				MPI_Isend(&randomVertex, 1, MPI_INT, r, 0, MPI_COMM_WORLD, &request);
-				s++;
-				if (s == graphNodes)
-					break;
+		int k_hat = 2;
+		int sendSize = k_hat +1 ;
+		int send_buf[4];// = (int*)malloc((k_hat + 1) * sizeof(int));
+		while (k_hat > 1) {
+			// MPI_Bcast(&k_hat, 1, MPI_INT, 0, MPI_COMM_WORLD);
+			int s = 0 ;
+			 
+			send_buf[0] = k_hat;
+			int R = comm_size;
+			while( s < graphNodes){
+				for ( int r = 1; r < comm_size; r++){
+					for (int k = 1; k <= k_hat; k++){
+						// int randomVertex = rand() % graphNodes;	
+						send_buf[k] = s;
+						printf("				MPI process %d sends value %d to processor %d.\n", my_rank, s, r);
+						s++;
+					}					
+					MPI_Send(&send_buf, sendSize, MPI_INT, r, 0, MPI_COMM_WORLD); // , &request			 , &sRequest		
+					if (s + k_hat >= graphNodes && s != graphNodes){
+						printf("				MPI process %d finds common neighbour for graphNodes - s vertices.\n", my_rank);
+						R = r + 1;
+						s = graphNodes;
+						break;
+					}
+					// MPI_Wait(&sRequest, MPI_STATUS_IGNORE);
+				}
+				// MPI_Gather(&edgeRemoved, 1, MPI_INT, edgesRemoved, 1, MPI_INT, 0, MPI_COMM_WORLD); //, &gRequest
+			    // MPI_Wait(&gRequest, MPI_STATUS_IGNORE);
+				for ( int r = 1; r < R; r++){
+					MPI_Recv(&edgeRemoved, 1, MPI_INT, r, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+					edgesRemoved[r] = edgeRemoved;
+					printf("Edges removed by processor %d: %d \n", r, edgesRemoved[r]);
+				}
+				printf("\n");
 			}
-			//MPI_Barrier(MPI_COMM_WORLD);
+			 
+			// calculate k_hat
+			k_hat = 1;
 		}
-		k_hat = 1;
-		MPI_Bcast(&k_hat, 1, MPI_INT, 0, MPI_COMM_WORLD);
 		printf("Sending terminating request\n");
-		// for (int c = 0; c < comm_size; c++){
-			// MPI_Isend(&termination, 1, MPI_INT, c, 0, MPI_COMM_WORLD, &request);
-		// }
-		
+		send_buf[0] = k_hat;
+		for ( int r = 1; r < comm_size; r++){
+			MPI_Isend(&send_buf, k_hat, MPI_INT, r, 0, MPI_COMM_WORLD, &sRequest); // , &request					
+		}
+		MPI_Wait(&sRequest, MPI_STATUS_IGNORE);
+		// free(send_buf);
+		// MPI_Bcast(&k_hat, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	}
 	else{
 			MPI_Request request;
 			int received;
+			int k_hat = 2;
+			int edgeRemoved;
+			int recv_buf[4]; // = (int*)malloc((k_hat + 1) * sizeof(int));
 			while(1){
 				
-				MPI_Bcast(&k_hat, 1, MPI_INT, 0, MPI_COMM_WORLD);
-				printf("MPI process %d received k_hat value: %d.\n", my_rank, k_hat);
+				// MPI_Bcast(&k_hat, 1, MPI_INT, 0, MPI_COMM_WORLD);				
+				MPI_Recv(&recv_buf, 3, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+				k_hat = recv_buf[0];
+				printf("MPI process %d waiting with k_hat %d.\n", my_rank, k_hat);
 				if (k_hat < 2)
 					break;
-				MPI_Recv(&received, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-				//MPI_Wait(&request, MPI_STATUS_IGNORE);
-
-				printf("MPI process %d received value: %d.\n", my_rank, received);
-				int p = (int) floor((received * graphNodes)/arraySize);
-				int idx = (received * graphNodes) % arraySize;
-				printf("Accesing processor %d and index %d \n", p, idx);
-				int targetIdx = (p - my_rank) * arraySize + idx;
-				printf("Processor %d target Id %d \n", my_rank, targetIdx);
-				for (int i = targetIdx; i < (graphNodes +  targetIdx); i++) { 
-					adjMatrix_buffer[i] = my_rank;
-					printf("%d ", adjMatrix_buffer[i]);
+				for(int k = 1; k <= k_hat; k++){
+					received = recv_buf[k];
+					printf("	MPI process %d received value: %d.\n", my_rank, received);
+					int p = (int) floor((received * graphNodes)/arraySize);
+					int idx = (received * graphNodes) % arraySize;
+					printf("Accesing processor %d and index %d \n", p, idx);
+					int targetIdx = (p - my_rank) * arraySize + idx;
+					printf("Processor %d target Id %d \n", my_rank, targetIdx);
+					for (int i = targetIdx; i < (graphNodes +  targetIdx); i++) { 
+						adjMatrix_buffer[i] = my_rank;
+						printf("%d ", adjMatrix_buffer[i]);
+					}
+					printf("\n");
 				}
-				printf("\n");
+				edgeRemoved = my_rank;
+				MPI_Isend(&edgeRemoved, 1, MPI_INT, 0, 0, MPI_COMM_WORLD, &request);
+				printf("k_hat %d \n", k_hat);				
 			}
+			// free(recv_buf);
 			printf("Processor %d terminating as (k_hat) %d < 2 \n", my_rank, k_hat);
 	}
 
