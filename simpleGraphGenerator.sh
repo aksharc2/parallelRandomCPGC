4165f6f0c02c0a6bf10da15b80e0db0c0a9fc05a
aksharc2 <aksharc22@gmail.com>
2024-04-28 17:46:22 -0400

    Added the graph generator files

---
+++ simpleGraphGenerator.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+# Job name
+#SBATCH --job-name parallel_random_CPGC
+
+# Submit to the primary QoS
+
+#SBATCH -p RM
+
+# Request one node
+
+#SBATCH -N 1
+
+# Total number of cores
+
+#SBATCH -n 1
+
+# Mail when the job begins, ends, fails, requeues
+
+#SBATCH --mail-type=ALL
+
+# Where to send email alerts
+
+#SBATCH --mail-user=gw4590@wayne.edu
+
+# Create an output file that will be output_<jobid>.out
+
+#SBATCH -o output_%j.out
+
+# Create an error file that will be error_<jobid>.out
+
+#SBATCH -e errors_%j.err
+
+# Set maximum time limit
+
+#SBATCH -t 72:00:00
+
+# Set minWall Time
+#SBATCH --time-min=48:00:00
+
+#cd /wsu/home/gw/gw45/gw4590/Graph_Compression/dataset1/
+#cd /your current directory/Graph_Compression/datasets/
+#echo "nodes,density,experimentNo"
+
+set -x
+
+for node in  131072 #262144 # 32768 32 64 128 256 512 1024 2048 4096 8192 65536
+do
+    for density in 98 #90 95  
+    do
+	for exp in 10 #1 2 3 4 5 6 7 8 9
+	do
+	     python3 simpleGraphGenerator.py $node $density $exp
+	done
+    done
+done
